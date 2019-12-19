b3a62da0af40d0fc3df262ae8bc6bca3c05e1d74
Andra <andracojocariu19@yahoo.com>
2019-12-19 20:40:23 +0200

    hh

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x;
+                x = 4;
+                      x++;
+
+        System.out.println(x);
+
+
+
+    }
+}
