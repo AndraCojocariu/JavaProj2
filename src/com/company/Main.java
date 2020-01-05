8ad7a2cb8a447befcbc53fbf672f9cbb9cb69017
Andra <andracojocariu19@yahoo.com>
2020-01-05 10:17:53 +0200

    tema primul lab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,41 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-
+    //1
     public static void main(String[] args) {
-        int x;
-                x = 4;
-                      x++;
-
-        System.out.println(x);
+        System.out.println("Hello");
+        System.out.println("My name is Andra");
 
 
+        //2
 
+        int x = 5;
+        float y = 8.96f;
+        float sum = x + y;
+        System.out.println("The result is: " + sum);
+
+        //3
+
+        float division = y / 5;
+        System.out.println("Rezultatul impartirii este: " + division);
+
+        //4
+
+        System.out.println(-5+(8*6));
+        System.out.println((55+9)%9);
+        System.out.println(20+((-3*5)/8));
+        System.out.println(5+((15/3)*2)-(8%3));
+
+        //declaring variables (la punctul c) )
+        int a = 20;
+        int b = -3;
+        int c = 5;
+        int d = 8;
+        System.out.println(a+((b*c)/d));
     }
 }
+
+
+
